27e1eaa5c87cc90bcd61c38e048f6e8922fe9cab
joy <jofficial248@gmail.com>
2022-09-19 21:06:30 +0600

    updated model

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,6 +1,9 @@
+from tkinter import Image
 from django.contrib import admin
-from .models import Category, Medicine, Brand
+from .models import Category, Inventory, Medicine, Brand, Image
 # Register your models here.
 admin.site.register(Medicine)
 admin.site.register(Brand)
 admin.site.register(Category)
+admin.site.register(Inventory)
+admin.site.register(Image)
--- inventory/models.py
+++ inventory/models.py
@@ -1,4 +1,4 @@
-from tabnanny import verbose
+
 from django.db import models
 
 # Create your models here.
@@ -11,24 +11,46 @@ class Brand(models.Model):
     
 class Category(models.Model):
     category_name = models.CharField(max_length=270)
-
+    slug = models.SlugField(null=True)
+    is_active = models.BooleanField(default=False)
     class Meta:
         verbose_name_plural = 'Categories'
     def __str__(self) -> str:
         return super().__str__(self.category_name)
 
+
+#product
 class Medicine(models.Model):
     medicine_name = models.CharField(max_length=270)
-    quantity = models.IntegerField()
-    price = models.FloatField()
-    is_active = models.BooleanField(default=True)
-    storage_date = models.DateField(auto_now=True)
+    description = models.CharField(max_length=300,null=True)
+    slug = models.SlugField(null=True)
+    med_category_name = models.ManyToManyField(Category,null=True)
+    med_brand_name = models.ForeignKey(Brand,on_delete=models.CASCADE,null=True)
+   
+    
+    
 
-
-    class Meta:
-        ordering = ['storage_date']
+    
     def __str__(self) -> str:
         return super().__str__(self.medicine_name)
 
 
 
+class Inventory(models.Model):
+    inventory_id = models.CharField(max_length=10,unique=True,primary_key=True)
+    product_name = models.ForeignKey(Medicine,on_delete=models.CASCADE)
+    inventory_price = models.DecimalField(max_digits=7,decimal_places=3)
+    unit_price = models.DecimalField(max_digits= 5,decimal_places=2)
+    is_active = models.BooleanField(default = False)
+    created_at = models.DateTimeField(auto_now_add = True, editable = False)
+    quantity = models.IntegerField()
+    promotion_price = models.DecimalField(max_digits=5,decimal_places=2)
+    promotion_is_active = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return super().__str__(self.product_name)
+
+class Image(models.Model):
+    url = models.FileField(upload_to=None)
+    alternative_text = models.CharField(max_length=100)
+    inventory = models.ForeignKey(Inventory,on_delete=models.CASCADE)
